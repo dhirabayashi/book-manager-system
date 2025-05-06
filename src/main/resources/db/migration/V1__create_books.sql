fc837ce67b08a5f0b9b23c65ec0841d6035d8942
daiki <gigabowser@hotmail.co.jp>
2025-05-06 13:51:28 +0900

    カラム長が小さすぎたため修正

--- src/main/resources/db/migration/V1__create_books.sql
+++ src/main/resources/db/migration/V1__create_books.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS books (
     id VARCHAR(26) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL,
-    publishing_status VARCHAR(10) NOT NULL CHECK (publishing_status IN ('UNPUBLISHED', 'PUBLISHED'))
+    publishing_status VARCHAR(255) NOT NULL CHECK (publishing_status IN ('UNPUBLISHED', 'PUBLISHED'))
 );
