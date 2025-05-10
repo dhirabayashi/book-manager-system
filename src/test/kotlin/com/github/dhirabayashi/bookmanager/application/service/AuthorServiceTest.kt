916cb647cebc9491946487f507d2118767801242
daiki <gigabowser@hotmail.co.jp>
2025-05-10 16:11:03 +0900

    Serviceのテストを追加

---
+++ src/test/kotlin/com/github/dhirabayashi/bookmanager/application/service/AuthorServiceTest.kt
@@ -0,0 +1,93 @@
+package com.github.dhirabayashi.bookmanager.application.service
+
+import com.github.dhirabayashi.bookmanager.application.exception.EntityNotFoundException
+import com.github.dhirabayashi.bookmanager.domain.model.Author
+import com.github.dhirabayashi.bookmanager.domain.reposiroty.AuthorRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.time.LocalDate
+
+@ExtendWith(MockitoExtension::class)
+class AuthorServiceTest {
+
+    @Mock
+    private lateinit var authorRepository: AuthorRepository
+
+    @InjectMocks
+    private lateinit var sut: AuthorService
+
+    @Test
+    @DisplayName("著者一覧が取得できること")
+    fun listAll() {
+        val authors = listOf(
+            Author.create(id = "id", name = "Author One", birthDate = LocalDate.of(1990, 1, 1)),
+            Author.create(id = "id2", name = "Author Two", birthDate = LocalDate.of(1990, 1, 2)),
+        )
+        whenever(authorRepository.findAll()).thenReturn(authors)
+
+        val result = sut.listAll()
+
+        assertThat(result).hasSize(2)
+        assertThat(result[0].id).isEqualTo("id")
+        assertThat(result[0].name).isEqualTo("Author One")
+        assertThat(result[0].birthDate).isEqualTo(LocalDate.of(1990, 1, 1))
+        assertThat(result[1].id).isEqualTo("id2")
+        assertThat(result[1].name).isEqualTo("Author Two")
+        assertThat(result[1].birthDate).isEqualTo(LocalDate.of(1990, 1, 2))
+
+        verify(authorRepository, times(1)).findAll()
+    }
+
+    @Test
+    @DisplayName("追加された著者が返ってくること")
+    fun add() {
+        val author = Author.create(id = "id", name = "Author Two", birthDate = LocalDate.of(1980, 1, 1))
+        whenever(authorRepository.add(author)).thenReturn(author)
+
+        val result = sut.add(author)
+
+        assertThat(result.id).isEqualTo("id")
+        assertThat(result.name).isEqualTo("Author Two")
+        assertThat(result.birthDate).isEqualTo(LocalDate.of(1980, 1, 1))
+
+        verify(authorRepository, times(1)).add(author)
+    }
+
+    @Test
+    @DisplayName("更新した著者が返ってくること")
+    fun update() {
+        val author = Author.create(id = "author1", name = "Updated Author", birthDate = LocalDate.of(1985, 1, 1))
+        whenever(authorRepository.update(author.id!!, author)).thenReturn(author)
+
+        val result = sut.update(author)
+
+        assertThat(result.id).isEqualTo("author1")
+        assertThat(result.name).isEqualTo("Updated Author")
+        assertThat(result.birthDate).isEqualTo(LocalDate.of(1985, 1, 1))
+
+        verify(authorRepository, times(1)).update(author.id!!, author)
+    }
+
+    @Test
+    @DisplayName("存在しない著者が指定された場合は例外が投げられること")
+    fun update_notFound() {
+        val author =
+            Author.create(id = "nonexistent", name = "Nonexistent Author", birthDate = LocalDate.of(1985, 1, 1))
+        whenever(authorRepository.update(author.id!!, author)).thenReturn(null)
+
+        assertThrows<EntityNotFoundException> {
+            sut.update(author)
+        }
+
+        verify(authorRepository, times(1)).update(author.id!!, author)
+    }
+}
---
+++ src/test/kotlin/com/github/dhirabayashi/bookmanager/application/service/BookServiceTest.kt
@@ -0,0 +1,215 @@
+package com.github.dhirabayashi.bookmanager.application.service
+
+import com.github.dhirabayashi.bookmanager.application.exception.EntityNotFoundException
+import com.github.dhirabayashi.bookmanager.domain.enum.PublishingStatus
+import com.github.dhirabayashi.bookmanager.domain.model.Author
+import com.github.dhirabayashi.bookmanager.domain.model.Book
+import com.github.dhirabayashi.bookmanager.domain.reposiroty.AuthorRepository
+import com.github.dhirabayashi.bookmanager.domain.reposiroty.BookRepository
+import com.github.dhirabayashi.bookmanager.domain.service.BookDomainService
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.time.LocalDate
+
+@ExtendWith(MockitoExtension::class)
+class BookServiceTest {
+
+    @Mock
+    private lateinit var bookRepository: BookRepository
+
+    @Mock
+    private lateinit var authorRepository: AuthorRepository
+
+    @Mock
+    private lateinit var bookDomainService: BookDomainService
+
+    @InjectMocks
+    private lateinit var bookService: BookService
+
+    @Test
+    @DisplayName("著者に紐づく書籍一覧を取得できること")
+    fun retrieveBooksByAuthorId() {
+        // テストデータ準備
+        val books = createTestBooks()
+        val bookOneAuthors = createTestAuthors(listOf("author1"))
+        val bookTwoAuthors = createTestAuthors(listOf("author1", "author2"))
+
+        // モック設定
+        whenever(bookRepository.findByAuthorId("author1")).thenReturn(books)
+        whenever(authorRepository.findByIds(listOf("author1"))).thenReturn(bookOneAuthors)
+        whenever(authorRepository.findByIds(listOf("author1", "author2"))).thenReturn(bookTwoAuthors)
+
+        // 実行
+        val result = bookService.retrieveBooksByAuthorId("author1")
+
+        assertThat(result).hasSize(2)
+
+        // アサーション
+        assertBook(result[0], books[0])
+        assertBook(result[1], books[1])
+
+        assertAuthor(result[0].authors[0], bookOneAuthors[0])
+        assertAuthor(result[1].authors[0], bookTwoAuthors[0])
+        assertAuthor(result[1].authors[1], bookTwoAuthors[1])
+
+        verify(bookRepository, times(1)).findByAuthorId("author1")
+        verify(authorRepository, times(1)).findByIds(listOf("author1"))
+        verify(authorRepository, times(1)).findByIds(listOf("author1", "author2"))
+    }
+
+
+    @Test
+    @DisplayName("書籍を登録できること")
+    fun addBook() {
+        // テストデータ準備
+        val book = createTestBooks().first()
+        val createdBook = Book.create(
+            id = "newId",
+            title = book.title,
+            price = book.price,
+            authorIds = book.authorIds,
+            publishingStatus = book.publishingStatus
+        )
+
+        val authors = createTestAuthors(book.authorIds)
+
+        // モック設定
+        whenever(bookRepository.add(book)).thenReturn(createdBook)
+        whenever(authorRepository.findByIds(book.authorIds)).thenReturn(authors)
+
+        // 実行
+        val result = bookService.add(book)
+
+        // 検証
+        assertBook(result, createdBook)
+
+        assertThat(result.authors).hasSize(authors.size)
+        result.authors.forEachIndexed { index, authorDto ->
+            assertAuthor(authorDto, authors[index])
+        }
+
+        verify(bookRepository, times(1)).add(book)
+        verify(authorRepository, times(1)).findByIds(book.authorIds)
+    }
+
+    @Test
+    @DisplayName("ID未指定で更新を実行した場合は例外が投げられること")
+    fun updateBook_shouldThrowExceptionWhenIdIsNull() {
+        val book = Book.create(null, "Test Book", 1000, listOf("author1"), PublishingStatus.UNPUBLISHED)
+
+        assertThatThrownBy { bookService.update(book) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    @DisplayName("存在しないIDで更新を実行した場合は例外が投げられること")
+    fun updateBook_shouldThrowExceptionWhenBookNotFound() {
+        val book = Book.create("nonexistence", "Test Book", 1000, listOf("author1"), PublishingStatus.UNPUBLISHED)
+
+        whenever(bookRepository.findById(book.id!!)).thenReturn(null)
+
+        assertThatThrownBy { bookService.update(book) }
+            .isInstanceOf(EntityNotFoundException::class.java)
+
+        verify(bookRepository, times(1)).findById(book.id!!)
+    }
+
+    @Test
+    @DisplayName("更新実行時、更新データの整合性チェック処理が呼ばれていること")
+    fun updateBook_shouldCallCanUpdateBook() {
+        val book = createTestBooks().first()
+        val currentBook = book
+
+        whenever(bookRepository.findById(book.id!!)).thenReturn(currentBook)
+        whenever(bookDomainService.canUpdateBook(currentBook, book)).thenReturn(true)
+        whenever(bookRepository.update(book.id!!, book)).thenReturn(book)
+
+        // 実行
+        bookService.update(book)
+
+        verify(bookDomainService, times(1)).canUpdateBook(currentBook, book)
+    }
+
+    @Test
+    @DisplayName("書籍を更新できること")
+    fun updateBook_shouldUpdateBook() {
+        val book = createTestBooks().last()
+        val currentBook = Book.create(
+            id = book.id,
+            title = "currentTitle",
+            price = 99999,
+            authorIds = listOf("currentAuthor"),
+            publishingStatus = PublishingStatus.UNPUBLISHED,
+        )
+
+        whenever(bookRepository.findById(book.id!!)).thenReturn(currentBook)
+        whenever(bookDomainService.canUpdateBook(currentBook, book)).thenReturn(true)
+        whenever(bookRepository.update(book.id!!, book)).thenReturn(book)
+
+        val authors = createTestAuthors(book.authorIds)
+        whenever(authorRepository.findByIds(book.authorIds)).thenReturn(authors)
+
+        // 実行
+        val result = bookService.update(book)
+
+        assertBook(result, book)
+
+        assertThat(authors).hasSize(authors.size)
+        result.authors.forEachIndexed { index, authorDto ->
+            assertAuthor(authorDto, authors[index])
+        }
+
+        verify(bookRepository, times(1)).findById(book.id!!)
+        verify(bookDomainService, times(1)).canUpdateBook(currentBook, book)
+        verify(bookRepository, times(1)).update(book.id!!, book)
+        verify(authorRepository, times(1)).findByIds(book.authorIds)
+    }
+
+    private fun createTestBooks(): List<Book> = listOf(
+        Book.create(
+            id = "book1",
+            title = "Test Book",
+            price = 1000,
+            authorIds = listOf("author1"),
+            publishingStatus = PublishingStatus.UNPUBLISHED
+        ),
+        Book.create(
+            id = "book2",
+            title = "Test Book2",
+            price = 1200,
+            authorIds = listOf("author1", "author2"),
+            publishingStatus = PublishingStatus.PUBLISHED
+        )
+    )
+
+    private fun createTestAuthors(ids: List<String>): List<Author> = ids.mapIndexed { i, id ->
+        Author.create(
+            id = id,
+            name = "Author $id",
+            birthDate = LocalDate.of(1990 + i, 1, 1)
+        )
+    }
+
+    private fun assertBook(actual: BookWithAuthorsDto, expected: Book) {
+        assertThat(actual.id).isEqualTo(expected.id)
+        assertThat(actual.title).isEqualTo(expected.title)
+        assertThat(actual.price).isEqualTo(expected.price)
+        assertThat(actual.publishingStatus).isEqualTo(expected.publishingStatus)
+        assertThat(actual.authors.map { it.id }).isEqualTo(expected.authorIds)
+    }
+
+    private fun assertAuthor(actual: AuthorDto, expected: Author) {
+        assertThat(actual.id).isEqualTo(expected.id)
+        assertThat(actual.name).isEqualTo(expected.name)
+        assertThat(actual.birthDate).isEqualTo(expected.birthDate)
+    }
+}
