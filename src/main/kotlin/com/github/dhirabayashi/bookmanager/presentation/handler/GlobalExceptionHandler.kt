e3798f4d2a188a117ce5850ac02ae28e033cc2eb
daiki <gigabowser@hotmail.co.jp>
2025-05-11 19:26:08 +0900

    想定外の例外が発生した場合にスタックトレースがレスポンスとして返らないようにする

--- src/main/kotlin/com/github/dhirabayashi/bookmanager/presentation/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/github/dhirabayashi/bookmanager/presentation/handler/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.github.dhirabayashi.bookmanager.presentation.handler
 
 import com.github.dhirabayashi.bookmanager.application.exception.EntityNotFoundException
 import com.github.dhirabayashi.bookmanager.domain.check.ValidationException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
+    companion object {
+        private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+    }
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleBookNotFound(ex: EntityNotFoundException): ResponseEntity<Map<String, String>> {
+        logger.error("error", ex)
+
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
             .body(mapOf("error" to (ex.message ?: "entity not found")))
@@ -19,8 +25,19 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(ValidationException::class)
     fun handleBadRequest(ex: ValidationException): ResponseEntity<Map<String, String>> {
+        logger.error("error", ex)
+
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(mapOf("error" to (ex.message ?: "invalid parameter")))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGeneralException(ex: Exception): ResponseEntity<Map<String, String>> {
+        logger.error("error", ex)
+
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(mapOf("error" to (ex.message ?: "internal server error")))
     }
 }
